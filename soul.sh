1afd4ff6e6e77ef9e1e98cd84190fe9173e904c5
GenZSouL <priyanshusinghal0818ext@gmail.com>
2023-07-10 10:32:06 +0000

    GenZSouL: Inital GenZSouL kernel bringup
    Signed-off-by: GenZSouL <priyanshusinghal0818ext@gmail.com>

---
+++ soul.sh
@@ -0,0 +1,76 @@
+DIR=`readlink -f .`
+MAIN=`readlink -f ${DIR}/..`
+export CLANG_PATH=$MAIN/clang-r416183b/bin
+export PATH=${BINUTILS_PATH}:${CLANG_PATH}:${PATH}
+make -j8 CC='ccache clang' ARCH=arm64 LLVM=1 LLVM_IAS=1 O=out gki_defconfig
+#!/bin/bash
+# Resources
+THREAD="-j$(nproc --all)"
+
+export CLANG_PATH=$MAIN/clang-r416183b/bin/
+export PATH=${CLANG_PATH}:${PATH}
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export CROSS_COMPILE=$MAIN/clang-r416183b/bin/aarch64-linux-gnu- CC=clang CXX=clang++
+
+DEFCONFIG="gki_defconfig"
+
+# Paths
+KERNEL_DIR=`pwd`
+ZIMAGE_DIR="$KERNEL_DIR/out/arch/arm64/boot"
+
+# Vars
+export ARCH=arm64
+export SUBARCH=$ARCH
+export KBUILD_BUILD_USER=Priyanshu
+
+MAKE="./makeparallel"
+
+DATE_START=$(date +"%s")
+
+echo  "DEFCONFIG SET TO $DEFCONFIG"
+echo "-------------------"
+echo "Making Kernel:"
+echo "-------------------"
+echo
+
+mkdir -p out
+make O=out clean
+
+make CC="ccache clang" CXX="ccache clang++" LLVM=1 LLVM_IAS=1 O=out $DEFCONFIG
+make CC='ccache clang' CXX="ccache clang++" LLVM=1 LLVM_IAS=1 O=out $THREAD \
+    CONFIG_MEDIATEK_CPUFREQ_DEBUG=m CONFIG_MTK_IPI=m CONFIG_MTK_TINYSYS_MCUPM_SUPPORT=m \
+    CONFIG_MTK_MBOX=m CONFIG_RPMSG_MTK=m CONFIG_LTO_CLANG=y CONFIG_LTO_NONE=n \
+    CONFIG_LTO_CLANG_THIN=y CONFIG_LTO_CLANG_FULL=n 2>&1 | tee kernel.log
+
+echo
+echo "-------------------"
+echo "Build Completed in:"
+echo "-------------------"
+echo
+
+DATE_END=$(date +"%s")
+DIFF=$(($DATE_END - $DATE_START))
+echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+echo
+ls -a $ZIMAGE_DIR
+
+# Verify Files
+
+echo "**** Verify Image.gz & dtbo.img ****"
+ls $PWD/out/arch/arm64/boot/Image.gz
+ls $PWD/out/arch/arm64/boot/dtbo.img
+ls $PWD/out/arch/arm64/boot/dtb.img
+
+cd $KERNEL_DIR
+
+TIME="$(date "+%Y%m%d-%H%M%S")"
+mkdir -p tmp
+cp -fp $ZIMAGE_DIR/Image.gz tmp
+cp -rp ./anykernel/* tmp
+cd tmp
+7za a -mx9 tmp.zip *
+cd ..
+rm *.zip
+cp -fp tmp/tmp.zip GenZSouL-xaga-$TIME.zip
+rm -rf tmp
+echo $TIME
